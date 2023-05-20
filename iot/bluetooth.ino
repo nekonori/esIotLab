b9634e48b3fdd766780507f2711c34d6d69be61e
Pragadesh BS <71137576+PragadeshBS@users.noreply.github.com>
2023-05-20 05:50:24 +0530

    Update bluetooth.ino

--- iot/bluetooth.ino
+++ iot/bluetooth.ino
@@ -1,28 +1,27 @@
-int led_pin = 2;
-void setup()
-{
-    pinMode(led_pin, OUTPUT);
-    Serial.begin(9600);
+#include <SoftwareSerial.h>
+
+// Replace with your Bluetooth module pins
+const int BT_TX_PIN = 2;
+const int BT_RX_PIN = 3;
+
+SoftwareSerial bluetooth(BT_RX_PIN, BT_TX_PIN); // RX, TX
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize Bluetooth Serial connection
+  bluetooth.begin(9600);
 }
-void loop()
-{
-    if (Serial.available())
-    {
-        char data_received;
-        data_received = Serial.read();
-        if (data_received == 'O')
-        {
-            digitalWrite(led_pin, HIGH);
-            Serial.write("LED is now ON!\n");
-        }
-        else if (data_received == 'X')
-        {
-            digitalWrite(led_pin, LOW);
-            Serial.write("LED is now OFF!\n");
-        }
-        else
-        {
-            Serial.write("Specify correct option\n");
-        }
-    }
+
+void loop() {
+  // Send data to Bluetooth module
+  bluetooth.println("Hello, world!");
+
+  // Wait for incoming data from Bluetooth module
+  while (bluetooth.available()) {
+    char incomingData = bluetooth.read();
+    Serial.print(incomingData);
+  }
+
+  delay(1000); // Wait for 1 second before sending again
 }
